7b947c2ad64c2f60c458b9e8a8bf8bc80e41cced
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-11-16 22:02:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,17 +2,28 @@
 
 int main(int ac, char **argv)
 {
-  char *line = NULL;
-  char **command = NULL;
+  char *line = NULL, **command = NULL;
   int status = 0;
   (void)ac;
+  
   while (1)
   {
     line = read_line();
     if (line == NULL)
+    {
+      if (isatty(STDIN_FILENO))
+      write(STDOUT_FILENO, "\n", 1);
+    
           return (status);
+    }
     command = tokenizer(line);
+    if (!command)
+    	continue;
+    
+    
     status = _execute(command, argv);
+    
+    
   }    
   
 }
