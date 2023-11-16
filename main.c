cff6585951fd95b46dcd61e878964bcd4af7e12d
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-11-16 01:53:09 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "shell.h"
 
+int handle_exit_builtin(char *command) {
+    if (strcmp(command, "exit") == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+int handle_env_builtin(char *command) {
+    if (strcmp(command, "env") == 0) {
+        char **env = environ;
+        while (*env != NULL) {
+            printf("%s\n", *env);
+            env++;
+        }
+        return 1;
+    }
+    return 0;
+}
+
 int main(void) {
-    char *command;
+    char *input;
+    char **args;
 
     while (1) {
-        display_prompt(); // Display the shell prompt
-        command = read_command(); // Read user input
+        display_prompt();
+        input = read_command();
 
-        if (command == NULL) {
+        if (input == NULL) {
             printf("\n");
             break; // Handle end of file condition (Ctrl+D)
         }
 
-        execute_command(command); // Execute the command
+        args = parse_input(input);
 
-        free(command); // Free allocated memory for command
+        if (handle_exit_builtin(args[0])) {
+            free(input);
+            free(args);
+            break;
+        }
+
+        if (handle_env_builtin(args[0])) {
+            free(input);
+            free(args);
+            continue;
+        }
+
+        char *full_command = find_command(args[0]);
+        if (full_command != NULL) {
+            execute_command(full_command, args);
+            free(full_command);
+        } else {
+            fprintf(stderr, "./shell: %s: command not found\n", args[0]);
+        }
+
+        free(input);
+        free(args);
     }
 
-    handle_exit(); // Handle the exit built-in command
-
+    // Handle exit
     return EXIT_SUCCESS;
 }
