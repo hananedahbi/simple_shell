5ff15e66ff02893b3c679157d2b38fd08e45899d
Hananedahbi123 <133127390+hananedahbi@users.noreply.github.com>
2023-11-16 14:42:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,13 @@ int main(int ac, char **argv)
 {
   char *line = NULL;
   char **command = NULL;
-  int status;
+  int status = 0;
   (void)ac;
   while (1)
   {
     line = read_line();
+    if (line == NULL)
+          return (status);
     command = tokenizer(line);
     status = _execute(command, argv);
   }    
